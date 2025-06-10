a46dce49f0ae247afc2c3f27988839974c73cf24
Bizzo <talnati@gmail.com>
2025-06-10 12:13:55 +0300

    adding pwa capabilities

---
+++ static/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'shatool-cache-v1';
+const urlsToCache = [
+    '/',
+    '/static/logo_icon.png',
+    '/static/logo_sidebar.png',
+    'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+}); 
